ca0c97808bb7e899ed748698ff17cb44a4643b58
chahid02 <chahid02@gmail.com>
2022-01-27 18:18:19 +0100

    first commit

---
+++ build/CMakeFiles/fernlere.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fernlere.dir/main.c.o"
+  "fernlere"
+  "fernlere.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fernlere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
