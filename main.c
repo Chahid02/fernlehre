555a9005e4751dd2266a71aee189a26965f8c405
chahid02 <chahid02@gmail.com>
2022-01-27 20:00:56 +0100

    threads created for UI_INTERFACE

--- main.c
+++ main.c
@@ -5,14 +5,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
 #include "main.h"
 #include "gui.h"
 
-
 int main(int argc, char **argv)
 {
+    pthread_t threads[NUM_THREADS];
+    int threadCreate;
+    int i;
 
+    threadCreate = pthread_create(&threads[i], NULL, UI_INTERFACE, (void *)NULL);
+    if (threadCreate)
+    {
+        printf("[X] Error:unable to create thread, %dr\\n", threadCreate);
+        exit(-1);
+    }
+
+    while (1)
+    {
+
+        break; // Just to test
+    }
+
+    pthread_exit(NULL);
+}
+
+
+void *UI_INTERFACE(void *threadID)
+{
+    long thread_ID;
+    thread_ID = (long)threadID;
+    printf("[X] UI_INTERFACE ID, %ld started\r\n", thread_ID);
     while (1)
     {
         UI_start();
@@ -20,5 +45,5 @@ int main(int argc, char **argv)
         break; // Just to test
     }
 
-    return 0;
+    pthread_exit(NULL);
 }
--- main.h
+++ main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#define NUM_THREADS 2
 
 
-
+void *UI_INTERFACE(void *threadID);
 
 #endif
