dc307081a25f193fcc5b586f929f24b09882f68a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-01-28 18:52:28 -0500

    implemented frame storage

--- middleware.c
+++ middleware.c
@@ -23,3 +23,75 @@ uint8_t calcChecksum(char* data, uint16_t* checksumBuffer)
 
     return errCode;
 }
+
+uint8_t storeFrame(Frame* storageFrame, char* rawFrame)
+{
+    uint8_t errCode = 0;
+    uint8_t bufferPosition = 0;
+
+    if(rawFrame == NULL || storageFrame == NULL)
+    {
+        printf("NULL Pointer! \n");
+    }
+
+    if(strlen(rawFrame) != BYTES_FRAME_TOTAL)
+    {
+        printf("Invalid frame length! \n");
+    }
+
+    memcpy(&(storageFrame->msgId), rawFrame, BYTES_MSG_ID);
+    bufferPosition = BYTES_MSG_ID;
+    memcpy(&(storageFrame->ack), rawFrame + bufferPosition, BYTES_ACK);
+    bufferPosition += BYTES_ACK;
+    memcpy(&(storageFrame->peerNr), rawFrame + bufferPosition, BYTES_PEER_NR);
+    bufferPosition += BYTES_PEER_NR;
+    memcpy(&(storageFrame->payloadLength), rawFrame + bufferPosition, BYTES_PAYLOAD_LENGTH);
+    bufferPosition += BYTES_PAYLOAD_LENGTH;
+    memcpy(&(storageFrame->payload), rawFrame + bufferPosition, BYTES_PAYLOAD);
+    bufferPosition += BYTES_PAYLOAD;
+    storageFrame->payload[BYTES_PAYLOAD] = '\0';
+    memcpy(&(storageFrame->checksum), rawFrame + bufferPosition, BYTES_CHECKSUM);
+
+    return errCode;
+}
+
+
+uint8_t createRawFrame(char rawFrame[BYTES_FRAME_TOTAL], uint8_t msgId, uint8_t ack, uint8_t peerNr, uint8_t payloadLength, char* inputData)
+{
+    uint8_t errCode = 0;
+    uint8_t bufferPosition = 0;
+    char payloadTemp[BYTES_PAYLOAD + 1];
+    uint16_t checksum;
+
+    rawFrame[0] = msgId;
+    bufferPosition += BYTES_MSG_ID;
+    rawFrame[bufferPosition] = ack;
+    bufferPosition += BYTES_ACK;
+    rawFrame[bufferPosition] = peerNr;
+    bufferPosition += BYTES_PEER_NR;
+    rawFrame[bufferPosition] = payloadLength;
+    bufferPosition += BYTES_PAYLOAD_LENGTH;
+
+    //copy inputData to payloadTemp 
+    for (uint8_t i = 0; i < payloadLength; i++)
+    {
+        payloadTemp[i] = inputData[i];
+        //rawFrame[bufferPosition + i] = inputData[i];
+    }
+    //fill the unused bytes of the payload with zeroes
+    for (uint8_t i = 0; i < BYTES_PAYLOAD - payloadLength; i++)
+    {
+        payloadTemp[payloadLength + i] = 0x00;
+    }
+    payloadTemp[BYTES_PAYLOAD] = '\0';
+
+    //copy all data bytes except the terminating char to rawFrame
+    memcpy(rawFrame+bufferPosition, payloadTemp, BYTES_PAYLOAD);
+
+    bufferPosition += BYTES_PAYLOAD;
+    errCode = calcChecksum(payloadTemp, &checksum);
+    memcpy(rawFrame+bufferPosition, &checksum, BYTES_CHECKSUM);
+
+
+    return errCode;
+}
--- middleware.h
+++ middleware.h
@@ -7,8 +7,26 @@
 #include <string.h>
 
 
-
+#define BYTES_MSG_ID 1
+#define BYTES_ACK 1
+#define BYTES_PEER_NR 1
+#define BYTES_PAYLOAD_LENGTH 1
 #define BYTES_PAYLOAD 32
+#define BYTES_CHECKSUM 2
+#define BYTES_TERM 1
+#define BYTES_FRAME_TOTAL (BYTES_MSG_ID + BYTES_ACK + BYTES_PEER_NR + BYTES_PAYLOAD + BYTES_CHECKSUM + BYTES_TERM) //38 Bytes total
+
+typedef struct
+{
+    uint8_t msgId;
+    uint8_t ack;
+    uint8_t peerNr;
+    uint8_t payloadLength;
+    char payload[BYTES_PAYLOAD + 1];
+    uint8_t checksum;
+}Frame;
+
+
 
 
 /*
@@ -18,6 +36,10 @@ length: length of data in bytes
 */
 
 uint8_t calcChecksum(char* data, uint16_t* checksumBuffer);
+
+uint8_t storeFrame(Frame* storageFrame, char* rawFrame);
+
+uint8_t createRawFrame(char rawFrame[BYTES_FRAME_TOTAL] , uint8_t msgId, uint8_t ack, uint8_t peerNr, uint8_t payloadLength, char* inputData);
 
 
 
