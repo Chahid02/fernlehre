79a3aefc3acf6ed440408f439d6cb7c92401fd58
chahid02 <chahid02@gmail.com>
2022-01-30 14:02:50 +0100

    payload case deleted

--- gui.c
+++ gui.c
@@ -196,11 +196,10 @@ void GUI_SELECTION(void)
     printf("[X] NUMBER:4 -----> GET PEER INFORMATION \n");
     printf("[X] NUMBER:5 -----> CREATE LOG FILE \n");
     printf("[X] NUMBER:6 -----> READ LOG FILE \n");
-    printf("[X] NUMBER:7 -----> DEFINE PAYLOAD SIZE \n");
-    printf("[X] NUMBER:8 -----> ERROR INJECTION BIT \n");
-    printf("[X] NUMBER:9 -----> SEND MESSAGE \n");
-    printf("[X] NUMBER:10 -----> SENT MESSAGES \n");
-    printf("[X] NUMBER:11 ----> RECEIVED MESSAGES \n");
+    printf("[X] NUMBER:7 -----> ERROR INJECTION BIT \n");
+    printf("[X] NUMBER:8 -----> SEND MESSAGE \n");
+    printf("[X] NUMBER:9 -----> SENT MESSAGES \n");
+    printf("[X] NUMBER:10 ----> RECEIVED MESSAGES \n");
     printf("---------------------------------------------\n");
     printf("[X] NOTE: INPUT BUFFER IS BROKEN AS FUCK !!!!\r\n");
     printf("[X] If you make a typing error, please press enter and type again !\r\n");
